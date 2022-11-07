3945ba65c8b921815937602812f4152f8fdea4a3
unknown <t1vian03@students,oamk.fi>
2022-11-07 12:45:28 +0200

    Closes #36, jwt init

--- server/app.js
+++ server/app.js
@@ -3,6 +3,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const jwt = require('jsonwebtoken');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const db = require('./database');
@@ -16,5 +18,28 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use(authenticateToken);
+
+
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+  
+    console.log("token = "+token);
+    if (token == null) return res.sendStatus(401)
+  
+    jwt.verify(token, process.env.MY_TOKEN, (err, user) => {
+      console.log(err)
+  
+      if (err) return res.sendStatus(403)
+  
+      req.user = user
+  
+      next()
+    })
+  }
+
 
 module.exports = app;
