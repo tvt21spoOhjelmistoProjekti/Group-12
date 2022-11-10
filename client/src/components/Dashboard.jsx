ac393f8bb722a5c21e35dcaf3c3aac9f3189f295
Ville <ville-simila@hotmail.com>
2022-11-10 16:29:06 +0200

    Frontpage wip

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { logOut, selectFullname, selectToken, selectUsername } from '../store/userSlice'
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+
 
 const Dashboard = () => {
     const token = useSelector(selectToken)
@@ -16,13 +19,40 @@ const Dashboard = () => {
         navigate("/")
     }
 
+
     return (
+        
         <div>
-            <p>{token}</p>
-            <p>{userName}</p>
-            <p>{fullname}</p>
-            <button onClick={temporaryLogoutButton}>Logout</button>
-        </div>
+            <div className='flex bg-bear bg-center bg-cover w-full h-screen'>
+                <div className='pl-12 pt-12'>
+                <DropdownButton id="dropdown-basic-button" title={" "+userName+" "}>
+                <Dropdown.Header>{fullname}</Dropdown.Header>
+                <Dropdown.Divider />
+                <Dropdown.Item href="#/action-2">Delete account</Dropdown.Item>
+                <Dropdown.Item onClick={temporaryLogoutButton}>Log out</Dropdown.Item>
+                </DropdownButton>
+                </div>
+
+                <div>
+                    <div className='Alaosa'>
+                        <div className='pt-32 items-center justify-center w-full content-center'>
+                            <div className='pl-32 pb-48 pt-20'>
+                                <button  className='bg-blue-600 text-white text-5xl w-96 h-36 rounded-full' type='submit'>My Visualizations</button>
+                            </div>
+                            <div className='pl-32 pb-8'>
+                                <button  className='bg-blue-600 text-white text-5xl w-96 h-36 rounded-full' type='submit'>Create new visualization</button>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        
+        
+    </div>
+        
+
+        
     )
 }
 
